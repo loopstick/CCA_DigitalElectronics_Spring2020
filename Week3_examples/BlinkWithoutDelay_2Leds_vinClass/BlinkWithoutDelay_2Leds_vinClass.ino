06b8f9bf1d052b12de112d417f3097c05d6c3dc4
Dr Sudhu <loopstick@hotmail.com>
2019-02-07 09:52:48 -0800

    cc

--- Week3_examples/BlinkWithoutDelay_2Leds_vinClass/BlinkWithoutDelay_2Leds_vinClass.ino
+++ Week3_examples/BlinkWithoutDelay_2Leds_vinClass/BlinkWithoutDelay_2Leds_vinClass.ino
@@ -31,21 +31,26 @@
 */
 
 // constants won't change. Used here to set a pin number:
-const int ledPin =  7;// the number of the LED pin
+const int led1Pin =  7;// the number of the LED pin
+const int led2Pin =  8;// the number of the LED pin
 
 // Variables will change:
-int ledState = LOW;             // ledState used to set the LED
+int led1State = LOW;             // ledState used to set the LED
+int led2State = LOW;             // ledState used to set the LED
 
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
+unsigned long previousMillis1 = 0;        // will store last time LED was updated
+unsigned long previousMillis2 = 0;        // will store last time LED was updated
 
 // constants won't change:
-const long interval = 1000;           // interval at which to blink (milliseconds)
+const long interval1 = 200;           // interval at which to blink (milliseconds)
+const long interval2 = 2f10;           // interval at which to blink (milliseconds)
 
 void setup() {
   // set the digital pin as output:
-  pinMode(ledPin, OUTPUT);
+  pinMode(led1Pin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -57,20 +62,43 @@ void loop() {
   // the interval at which you want to blink the LED.
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
+// #1 instance ++++++++++++++++++++++++++++++++++++
+  if (currentMillis - previousMillis1 >= interval1) {
     // save the last time you blinked the LED
-    previousMillis = currentMillis;
-    Serial.println(previousMillis);
+    previousMillis1 = currentMillis;
+    Serial.print("previous Millis 1 - ");
+    Serial.println(previousMillis1);
 
     // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
+    if (led1State == LOW) {
+      led1State = HIGH;
     } else {
-      ledState = LOW;
+      led1State = LOW;
     }
 
     // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
+    digitalWrite(led1Pin, led1State);
   }
+//++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// #2 instance ++++++++++++++++++++++++++++++++++++
+  if (currentMillis - previousMillis2 >= interval2) {
+    // save the last time you blinked the LED
+    previousMillis2 = currentMillis;
+    Serial.print("previous Millis 2 - ");
+    Serial.println(previousMillis2);
+
+    // if the LED is off turn it on and vice-versa:
+    if (led2State == LOW) {
+      led2State = HIGH;
+    } else {
+      led2State = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(led2Pin, led2State);
+  }
+//++++++++++++++++++++++++++++++++++++++++++++++++++++
+  
   //Serial.println(currentMillis);
 }
